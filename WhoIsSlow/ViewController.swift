dd03f1f790cb2c8bfcf7b9dfc1653c1ea7df8dbd
miroshniktyt <miroshniktyt@gmail.com>
2020-05-11 14:02:49 +0300

    Initial Commit

---
+++ WhoIsSlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhoIsSlow
+//
+//  Created by Anton Miroshnyk on 5/11/20.
+//  Copyright © 2020 Anton Miroshnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
